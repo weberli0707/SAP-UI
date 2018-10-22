636ab3eb334aa42426a3d61042f3ca87f2d632f7
weberli0707 <lijiehua123@163.com>
2018-10-22 13:14:36 +0800

    version:20181020

---
+++ WebContent/util/TableOperationslmpl.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+	"sap/ui/base/Object",
+	"sap/ui/model/Sorter",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
+], function(BaseObject,Sorter,Filter,FilterOperator) {
+	"use strict";
+	
+	return Object.extend("sap.ui.demoapps.rta.freestyle.util.TableOperationsImpl",{
+		
+	});
+});
